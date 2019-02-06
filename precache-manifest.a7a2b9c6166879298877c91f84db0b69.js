93bc2421a114d2ccf79e2525146a9b21d9dc355e
Darshanl <darshan.lakade157@gmail.com>
2019-02-06 20:34:48 +0530

    Updates

---
+++ precache-manifest.a7a2b9c6166879298877c91f84db0b69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "5d163b50db250cc8a57f",
+    "url": "/robofriends/static/js/main.5d163b50.chunk.js"
+  },
+  {
+    "revision": "c10b818867934b437dae",
+    "url": "/robofriends/static/js/1.c10b8188.chunk.js"
+  },
+  {
+    "revision": "5d163b50db250cc8a57f",
+    "url": "/robofriends/static/css/main.37d6adaf.chunk.css"
+  },
+  {
+    "revision": "c10b818867934b437dae",
+    "url": "/robofriends/static/css/1.8c0f2075.chunk.css"
+  },
+  {
+    "revision": "4ee8eaef73141c793d6ea8994e03c7db",
+    "url": "/robofriends/index.html"
+  }
+];
